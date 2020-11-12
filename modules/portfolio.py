cff3aec3f4b41a50cbdd0e53a573010a52a486b8
Babiole77 <n.yernar@gmail.com>
2020-11-13 00:14:12 +0100

    Created Portfolio class

--- modules/portfolio.py
+++ modules/portfolio.py
@@ -4,65 +4,78 @@ sys.path.insert(0,'modules')
 from database import select_data
 from orders import get_current_portfolio
 
-def is_in_potfolio(db_conn, isin):
-    """Check if an instrument is already in the portfolio.
+class Portfolio:
+    def __init__(self, api_conn):
+        self.__api_key = api_conn
 
-    Args:
-        db_conn (sqlite3.connect): database connection object
-        isin (string): isin of an instrument
+    def delete_order(order_uuid):
+        pass
 
-    Returns:
-        True or False
-    """
+    def get_current_portfolio():
+        pass
 
-    df_portfolio = get_current_portfolio()
-    if isin in df_portfolio['isin'].unique():
-        return True
-    else:
-        return False
+    def create_order():
+        pass
 
-def is_open_buy_order(db_conn, isin):
-    """Check if there is an open buy order for isin.
+    def is_in_potfolio(isin):
+        """Check if an instrument is already in the portfolio.
 
-    Args:
-        db_conn (sqlite3.connect): database connection object
-        isin (string): isin of an instrument
+        Args:
+            db_conn (sqlite3.connect): database connection object
+            isin (string): isin of an instrument
 
-    Returns:
-        status (boolean): True or False
-        df_open_buy (pandas df): df with open buy orders for this instrument
-    """
+        Returns:
+            True or False
+        """
 
-    sql_select_open_buy = """SELECT uuid, isin, type, quantity FROM open_buy_orders where isin = '{}'""".format(isin)
-    open_buy = select_data(db_conn, sql_select_open_buy)
-    df_open_buy = pd.DataFrame(open_buy, columns = ['order_uuid', 'isin', 'type', 'quantity'])
+        df_portfolio = get_current_portfolio()
+        if isin in df_portfolio['isin'].unique():
+            return True
+        else:
+            return False
 
-    if isin in df_open_buy['isin'].unique():
-        status =  True
-    else:
-        status =  False
+    def is_open_buy_order(db_conn, isin):
+        """Check if there is an open buy order for isin.
 
-    return status, df_open_buy
+        Args:
+            db_conn (sqlite3.connect): database connection object
+            isin (string): isin of an instrument
 
-def is_open_sell_order(db_conn, isin):
-    """Check if there is an open sell order for isin.
+        Returns:
+            status (boolean): True or False
+            df_open_buy (pandas df): df with open buy orders for this instrument
+        """
 
-    Args:
-        db_conn (sqlite3.connect): database connection object
-        isin (string): isin of an instrument
+        sql_select_open_buy = """SELECT uuid, isin, type, quantity FROM open_buy_orders where isin = '{}'""".format(isin)
+        open_buy = select_data(db_conn, sql_select_open_buy)
+        df_open_buy = pd.DataFrame(open_buy, columns = ['order_uuid', 'isin', 'type', 'quantity'])
 
-    Returns:
-        status (boolean): True or False
-        df_open_sell (pandas df): df with open sell orders for this instrument
-    """
+        if isin in df_open_buy['isin'].unique():
+            status =  True
+        else:
+            status =  False
 
-    sql_select_open_sell = """SELECT uuid, isin, type, quantity FROM open_sell_orders where isin = '{}'""".format(isin)
-    open_sell = select_data(db_conn, sql_select_open_sell)
-    df_open_sell = pd.DataFrame(open_sell, columns = ['order_uuid', 'isin', 'type', 'quantity'])
+        return status, df_open_buy
 
-    if isin in df_open_sell['isin'].unique():
-        status =  True
-    else:
-        status =  False
+    def is_open_sell_order(db_conn, isin):
+        """Check if there is an open sell order for isin.
 
-    return status, df_open_sell
+        Args:
+            db_conn (sqlite3.connect): database connection object
+            isin (string): isin of an instrument
+
+        Returns:
+            status (boolean): True or False
+            df_open_sell (pandas df): df with open sell orders for this instrument
+        """
+
+        sql_select_open_sell = """SELECT uuid, isin, type, quantity FROM open_sell_orders where isin = '{}'""".format(isin)
+        open_sell = select_data(db_conn, sql_select_open_sell)
+        df_open_sell = pd.DataFrame(open_sell, columns = ['order_uuid', 'isin', 'type', 'quantity'])
+
+        if isin in df_open_sell['isin'].unique():
+            status =  True
+        else:
+            status =  False
+
+        return status, df_open_sell
