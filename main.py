ad2c919f9e4772b70905a69174140da22c5238dd
Babiole77 <n.yernar@gmail.com>
2020-11-16 21:46:08 +0100

    Completed moving_average strategy

--- main.py
+++ main.py
@@ -1,65 +1,25 @@
-import threading
-import sys
 import configparser
+import sys
 
 sys.path.insert(0,'modules')
-from telegramBot import send_state
-from helpers import prices_cleanup
-from database import create_connection
+from strategy import Strategy
 from tradingBot import TradingBot
+from portfolio import Portfolio
+from database import Database
+from receiver import Receiver
+
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-headers = {
-  'APCA-API-KEY-ID': config['alpaca-paper']['APCA_API_KEY_ID'],
-  'APCA-API-SECRET-KEY': config['alpaca-paper']['APCA_API_SECRET_KEY']
-}
+db = config['database']['database_uri']
+db_conn = Database.create_connection(db)
+st = Strategy(db_conn, 'moving_average', 'limit', window_len = 5, lookback_len = 7, buy_threshold = 0.01, profit_margin = 0.05, stop_threshold = 0.05)
+acc = Portfolio('ua')
 
-def main():
+rec = Receiver('wss://data.alpaca.markets/stream', db)
+bot = TradingBot(db, st, acc, 30)
 
-  program_state = 'Shutdown'
-  print('Starting the program...')
-  config = configparser.ConfigParser()
-  config.read('config.ini')
-  db = config['sqlite']['database']
-
-  strategy_buy = {'algorithm':'moving_average', 
-                'period'        : config.getint('thread_params', 'period_buy'), 
-                'buy_amount'    : config.getint('algo_params', 'buy_amount'), 
-                'window_len'    : config.getint('algo_params', 'window_length'), 
-                'lookback_len'  : config.getint('algo_params', 'lookback_length'), 
-                'buy_threshold' : config.getfloat('algo_params', 'buy_threshold'),
-                'stop_loss'    : config.getfloat('algo_params', 'stop_loss_threshold'),
-                'profit_margin': config.getfloat('algo_params', 'profit_margin')}
-
-  strategy_sell = {'algorithm': 'no_algo'}
-  try:
-    # Create threads: receive and write to database; buy stocks; sell stocks; verify open orders
-    trading_bot = TradingBot(strategy_buy, strategy_sell)
-    trading_bot.start()
-
-    # watchdog for the program
-    while True:
-      number_of_threads = config.getint('thread_params', 'number_of_threads')
-      if program_state == 'Shutdown' and threading.active_count() == number_of_threads:
-        print('Program started normally')
-        program_state = 'Normal'
-        # send telegram notification
-        send_state('started')
-      if program_state == 'Normal' and threading.active_count() != number_of_threads:
-        print('An error occured. Program is terminating...')
-        trading_bot.stop()
-        program_state = 'Stopping'
-      if program_state == 'Stopping' and threading.active_count() == 1:
-        program_state = 'Shutdown'
-        print('Program terminated')
-        prices_cleanup(create_connection(db))
-        send_state('stopped')
-        break
-  except KeyboardInterrupt:
-    prices_cleanup(create_connection(db))
-    send_state('manually stopped')
-
-if __name__== "__main__":
-  main()
+rec.start()
+bot.start()
