366f24576985f9e53658c6b65242486e5f9fefa4
Babiole77 <n.yernar@gmail.com>
2020-11-16 18:16:14 +0100

    Dev fixes

--- modules/strategy.py
+++ modules/strategy.py
@@ -7,10 +7,9 @@ sys.path.insert(0,'modules')
 from database import Database
 
 class Strategy:
-    def __init__(self, db_conn, buy_strategy, sell_strategy, **kwargs):
+    def __init__(self, buy_strategy, sell_strategy, **kwargs):
         self.means = {}
         self.symbols_to_trade = []
-        self.db_conn = db_conn
         self.buy_strategy = buy_strategy
         self.sell_strategy = sell_strategy
         self.window_len = kwargs.get('window_len')
@@ -18,7 +17,7 @@ class Strategy:
         self.stop_threshold = kwargs.get('stop_threshold')
         self.profit_margin = kwargs.get('profit_margin')
     
-    def calculate_means(self, window_len, buffer_size, symbol):
+    def calculate_means(self, db_conn, window_len, buffer_size, symbol):
         """Calculates moving averages for the symbol
         The function shall be called periodically from the same place 
         Args:
@@ -31,11 +30,12 @@ class Strategy:
         Returns:
             means (np.array): moving averages for the symbol
         """
-        df = Database.get_last_n_prices(self.db_conn, symbol, window_len)
+        df = Database.get_last_n_prices(db_conn, symbol, window_len)
+        print(df)
         # if not enough data for moving average calculation or wrong timestamps
         if len(df.index) < window_len:
             raise ValueError("Not suitable data for moving average calculation")
-        mean = df['ask_price'].mean()
+        mean = df['close_price'].mean()
 
         # check if symbol appears for the first time
         if symbol in self.means:
@@ -51,7 +51,7 @@ class Strategy:
 
         return np.asarray(self.means[symbol])
 
-    def buy_strategy_first_momentum(self, window_len, lookback_len, profit_margin, stop_threshold):
+    def buy_strategy_first_momentum(self, db_conn, window_len, lookback_len, profit_margin, stop_threshold):
         print("Started first momentum")
         # do calculations for all symbol
         for symbol in ['AAPL', 'TSLA']:
@@ -77,7 +77,7 @@ class Strategy:
             
         return None
 
-    def buy_strategy_moving_average(self, window_len, lookback_len, buy_threshold):
+    def buy_strategy_moving_average(self, db_conn, window_len, lookback_len, buy_threshold):
         # do calculations for all symbol
         for symbol in ['AAPL', 'TSLA']:
             # ignore symbol if there is already open buy
@@ -95,7 +95,7 @@ class Strategy:
         
         return None
 
-    def get_symbols_to_trade(self):
+    def get_symbols_to_trade(self, db_conn):
         self.symbols_to_trade = []
         if self.buy_strategy == 'first_momentum' and self.sell_strategy == 'limit':
             window_len = self.window_len
@@ -103,4 +103,5 @@ class Strategy:
             profit_margin = self.profit_margin
             stop_threshold = self.stop_threshold
             self.buy_strategy_first_momentum(window_len, lookback_len, profit_margin, stop_threshold)
-        return self.symbols_to_trade 
+        return self.symbols_to_trade
+
