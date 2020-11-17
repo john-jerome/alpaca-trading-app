b92f3dbc5bb032d1c19d627be8441c7044a4c158
john-jerome <31763328+john-jerome@users.noreply.github.com>
2020-11-17 16:34:36 +0100

    Update receiver.py

--- modules/receiver.py
+++ modules/receiver.py
@@ -13,7 +13,6 @@ from database import Database
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-APCA_API_BASE_URL = config['alpaca-paper']['APCA_API_BASE_URL']
 APCA_API_KEY_ID = config['alpaca-paper']['APCA_API_KEY_ID']
 APCA_API_SECRET_KEY = config['alpaca-paper']['APCA_API_SECRET_KEY']
 
@@ -23,6 +22,7 @@ class Receiver:
         self.__stop_receiving_data = threading.Event()
         self.websocket_url = websocket_url
         self.database_uri = database_uri
+        self.db_conn = 0
 
     def start(self):
         receive_data_thread = threading.Thread(target = self.__receive_data, args=(self.__stop_receiving_data, 0,))
@@ -31,9 +31,9 @@ class Receiver:
     def stop(self):
         self.__stop_receiving_data.set()
     
-    def ws_authenticate(self):
+    def on_open(self, ws, conn):
 
-        payload = {
+        auth_payload = {
             "action": "authenticate",
             "data": {
                 "key_id": APCA_API_KEY_ID,
@@ -41,53 +41,50 @@ class Receiver:
                 }
             }
 
-        ws = websocket.WebSocket()
-        ws.connect(self.websocket_url)
+        ws.send(json.dumps(auth_payload))
+
+        am_symbols = ["AM." + e for e in Database.get_all_symbols(self.db_conn)]
+        payload = {
+            "action": "listen",
+            "data": {
+                "streams": am_symbols
+                }
+                }
+        
         ws.send(json.dumps(payload))
-        response = json.loads(ws.recv())
 
-        status = response['data']['status']
+    def on_message(self, ws, message):
 
-        return ws, status
+        print("received a message")
+        if 'AM' in message['stream']:
+            row = (
+                message['data']['ev'],
+                message['data']['T'],
+                message['data']['v'],
+                message['data']['av'],
+                message['data']['op'],
+                message['data']['vw'],
+                message['data']['o'],
+                message['data']['h'],
+                message['data']['l'],
+                message['data']['c'],
+                message['data']['a'],
+                unix_to_ts(message['data']['s'] / 1000.0),
+                unix_to_ts(message['data']['e'] / 1000.0)
+                )
+            Database.insert_one_row(self.db_conn, row, table_name = 'alpaca.prices_bars')
+            
+    def on_close(self, ws):
+        print("closed connection")
 
-    def __receive_data(self, stop_event, symbol_list):
+    def __receive_data(self):
 
-        conn = Database.create_connection(self.database_uri)
-        ws, status = self.ws_authenticate()
-        timeout = config.getint('alpaca-paper', 'websocket_timeout')
-        ws.settimeout(timeout)
+        websocket.enableTrace(True)
+        self.db_conn = Database.create_connection(self.database_uri)
+        while not self.__stop_receiving_data:
+            ws = websocket.WebSocketApp(
+                self.websocket_url, on_open=self.on_open, 
+                on_message=self.on_message, on_close=self.on_close
+                )
+            ws.run_forever()
 
-        if status == 'authorized':
-            am_symbols = ["AM." + e for e in Database.get_all_symbols(conn)]
-            payload = {
-                        "action": "listen",
-                        "data": {
-                            "streams": am_symbols
-                            }
-                        }
-            ws.send(json.dumps(payload))
-            with conn:
-                while not self.__stop_receiving_data.is_set():
-                    try:
-                        response = json.loads(ws.recv())
-                        print("Websocket: " + response['stream'])
-                        if response['stream'] != 'listening':
-                            row = (
-                                response['data']['ev'],
-                                response['data']['T'],
-                                response['data']['v'],
-                                response['data']['av'],
-                                response['data']['op'],
-                                response['data']['vw'],
-                                response['data']['o'],
-                                response['data']['h'],
-                                response['data']['l'],
-                                response['data']['c'],
-                                response['data']['a'],
-                                unix_to_ts(response['data']['s'] / 1000.0),
-                                unix_to_ts(response['data']['e'] / 1000.0)
-                            )
-                            Database.insert_one_row(conn, row, table_name = 'alpaca.prices_bars')
-                    except Exception as e:
-                        print("Websocket error:", e)
-                        break
