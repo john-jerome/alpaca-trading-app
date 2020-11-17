2fb0dafdc1436afa8a5a0f49c36faa8f3026c2ec
Babiole77 <n.yernar@gmail.com>
2020-11-17 14:32:48 +0100

    Added a watchdog and telegram notificaitons

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import configparser
 import sys
+import threading
 
 sys.path.insert(0,'modules')
+from telegramBot import send_state
 from strategy import Strategy
 from tradingBot import TradingBot
 from portfolio import Portfolio
 from database import Database
 from receiver import Receiver
-
-
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -18,8 +18,29 @@ db_conn = Database.create_connection(db)
 move_av = Strategy(db_conn, 'moving_average', 'limit', window_len = 5, lookback_len = 7, buy_threshold = 0.01, profit_margin = 0.05, stop_threshold = 0.05)
 account = Portfolio('ua')
 
-rec = Receiver('wss://data.alpaca.markets/stream', db)
-bot = TradingBot(move_av, account, period = 30)
+dataReceiver = Receiver('wss://data.alpaca.markets/stream', db)
+traderBot = TradingBot(move_av, account, period = 30)
 
-rec.start()
-bot.start()
+dataReceiver.start()
+traderBot.start()
+
+number_of_threads = 3
+program_state = 'Init'
+# watchdog for the program
+while True:
+  if program_state == 'Init' and threading.active_count() == number_of_threads:
+    print('Program started normally')
+    program_state = 'Normal'
+    # send telegram notification
+    send_state('started')
+  if program_state == 'Normal' and threading.active_count() != number_of_threads:
+    print('An error occured. Program is terminating...')
+    dataReceiver.stop()
+    traderBot.stop()
+    program_state = 'Stopping'
+  if program_state == 'Stopping' and threading.active_count() == 1:
+    break
+
+Database.close_connection(db_conn)
+print('Program terminated')
+send_state('stopped')
