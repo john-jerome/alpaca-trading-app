64e63e8e697337a517761a86b8999e1c5c7c8c9d
Babiole77 <n.yernar@gmail.com>
2020-11-21 18:52:00 +0100

    Cleaned up code, added clarificaitons

--- modules/tradingBot.py
+++ modules/tradingBot.py
@@ -26,13 +26,17 @@ class TradingBot():
         while is_market_open() and (not self.__stop_trading.is_set()):
             # if less than 30 minutes to market close then close all positions and stop the program
             if time_to_market_close() < 30:
+                print('Market will close soon. Sell all positions and close all positions before market close')
                 self.account.cancel_all_orders()
                 self.account.liquidate_all_positions()
                 self.stop()
             for trade in self.strategy.get_symbols_to_trade():
+                # ignore the symbol if it is in already in the portoflio or we have a pending open order 
                 if self.account.is_in_potfolio(trade['symbol']) or trade['symbol'] in self.account.get_open_orders('buy'):
                     continue
-                if trade['order_class'] == 'bracket':
+                if trade['order_class'] == 'simple':
+                    pass
+                elif trade['order_class'] == 'bracket':
                     buy_amount = config.getint('algo_params', 'buy_amount')
                     n_shares = math.floor(buy_amount/trade['limit_price'])
                     print("Creating a bracket order for {}...".format(trade['symbol']))
@@ -43,6 +47,13 @@ class TradingBot():
                         take_profit_limit_price=trade['limit_price'], 
                         stop_loss_stop_price=trade['stop_price']
                         )
+                elif trade['order_class'] == 'oco':
+                    pass
+                elif trade['order_class'] == 'oto':
+                    pass
+                else:
+                    print('Specified order class does not exist')
+                    self.stop()
             time.sleep(self.period)
 
         print('Stopped', self.account.account_id, 'trader bot')
